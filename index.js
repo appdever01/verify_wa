42e74f8cc7306695e4cff312a05c782a6cea7d33
Naheem <appdever01@gmail.com>
2024-07-04 14:20:41 +0100

    m

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const {
   default: Baileys,
-  delay,
   DisconnectReason,
   fetchLatestBaileysVersion,
   useMultiFileAuthState,
 } = require("@whiskeysockets/baileys");
+const P = require("pino");
 const { imageSync } = require("qr-image");
 const { Boom } = require("@hapi/boom");
 const express = require("express");
+const fs = require("fs-extra");
 const cors = require("cors");
 
 const app = express();
+const port = 3000;
+const verification = new Map();
+
+app.set("json spaces", 2);
 app.use(express.json());
 app.use(cors());
-const P = require("pino");
+
 const start = async () => {
   const { state, saveCreds } = await useMultiFileAuthState("session");
   const client = Baileys({
@@ -21,52 +26,48 @@ const start = async () => {
     printQRInTerminal: true,
     auth: state,
     logger: P({ level: "fatal" }),
-    browser: ["TG-WhatsApp", "fatal", "1.0.0"],
+    browser: ["AuthGuard-Bot", "fatal", "1.0.0"],
   });
 
-  client.ev.on("connection.update", async (update) => {
+  // Private Password to Your Qr code.
+  client.session = "123";
+
+  client.ev.on("connection.update", (update) => {
+    const { connection, lastDisconnect } = update;
     if (update.qr) {
       console.log(
-        `QR code generated. Scan it to continue | You can also authenticate in http://localhost:3000`
+        `QR code generated. Scan it to continue | You can also authenticate in http://localhost:${port}`
       );
       client.QR = imageSync(update.qr);
     }
-    const { connection, lastDisconnect } = update;
     if (connection === "close") {
       const { statusCode } = new Boom(lastDisconnect?.error).output;
       if (statusCode !== DisconnectReason.loggedOut) {
-        console.log("Connecting...");
-        setTimeout(() => start(), 3000);
+        console.log("Reconnecting...");
+        setTimeout(start, 3000);
       } else {
-        client.log("Disconnected.", "red");
-        await remove("session");
+        console.log("Disconnected.");
+        fs.removeSync("session");
         console.log("Starting...");
-        setTimeout(() => start(), 3000);
+        setTimeout(start, 3000);
       }
     }
-    if (connection === "connecting") console.log("Connecting to WhatsApp...");
+    if (connection === "connecting") {
+      client.condition = "connecting";
+      console.log("Connecting to WhatsApp...");
+    }
     if (connection === "open") {
+      client.condition = "connected";
       console.log("Connected to WhatsApp");
     }
   });
 
-  client.ev.on("messages.upsert", async ({ messages }) => {
-    const formatArgs = (args) => args.slice(1).join(" ").trim();
-    const M = messages[0];
-    M.from = M.key.remoteJid || "";
-    M.sender = M.key.participant || "";
-    M.content = M.message?.conversation || "";
-    M.reply = (text) => client.sendMessage(M.from, { text }, { quoted: M });
-    const args = M.content.split(" ");
-    const context = formatArgs(args);
-    M.reply("text");
-  });
-
   /**
-   * @param {string}
+   * @param {string} str
+   * @returns {string}
    */
 
-  client.makeWaJid = (str) =>
+  const correctJid = (str) =>
     (/\d/.test(str) ? str.replace(/\D/g, "") : 123) + "@s.whatsapp.net";
 
   /**
@@ -74,23 +75,50 @@ const start = async () => {
    * @returns {boolean}
    */
 
-  client.isWaNumber = async (phone) =>
+  const validWhatsApp = async (phone) =>
     (await client.onWhatsApp(phone))[0]?.exists || false;
 
-  const jid = client.makeWaJid("2347049972537");
-  const valid = await client.isWaNumber(jid);
-  console.log(jid);
-
-  console.log(valid);
-
   app.get("/", (req, res) =>
-    res.status(200).contentType("image/png").send(client.QR)
+    res.status(200).setHeader("Content-Type", "text/plain").send("Running...")
   );
+
+  app.get("/wa/qr", async (req, res) => {
+    const { session } = req.query;
+    if (!session || !client || client.session !== req.query.session)
+      return void res
+        .status(404)
+        .setHeader("Content-Type", "text/plain")
+        .send("Invalid Session")
+        .end();
+    if (!client || !client.QR)
+      return void res
+        .status(404)
+        .setHeader("Content-Type", "text/plain")
+        .send(
+          client.condition === "connected"
+            ? "You are already connected to WhatsApp"
+            : "QR not generated"
+        )
+        .end();
+    res.status(200).contentType("image/png").send(client.QR);
+  });
+
+  app.all("/request", async (req, res) => {
+    const { phone } = req.method === "GET" ? req.query : req.body;
+    if (!phone) return void res.sendStatus(404);
+    const jid = correctJid(phone);
+    const valid = await validWhatsApp(jid);
+    if (!valid)
+      return void res
+        .status(404)
+        .json({ error: "Number not available on WhatsApp" });
+  });
+
+  app.all("*", (req, res) => res.sendStatus(404));
 
   client.ev.on("creds.update", saveCreds);
   return client;
 };
-app.listen(3000, () => {
-  console.log(`Server started on PORT : 3000`);
-  start();
-});
+
+start();
+app.listen(port, () => console.log(`Server started on PORT: ${port}`));
