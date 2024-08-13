3c8cc305b50540ec22aa22c0e4b9b30a38fdee18
appdever01 <appdever01@gmail.com>
2024-08-13 06:43:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -144,7 +144,6 @@ app.delete("/deleteFiles", (req, res) => {
   });
 });
 app.get("/progress", (req, res) => {
-  workbook = xlsx.readFile(`uploads/updated_file.xlsx`);
   const sheet_name_list = workbook.SheetNames;
   const sheet1Data = xlsx.utils.sheet_to_json(
     workbook.Sheets[sheet_name_list[0]]
